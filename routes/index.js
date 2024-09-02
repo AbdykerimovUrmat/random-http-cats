8037bde1b6a1170fb4ab5f038453a21e08396e1a
Urmat Abdykerimov <abdykerimov.urm@gmail.com>
2024-09-02 15:27:46 +0300

    now images local

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,29 @@
 var express = require('express');
-const axios = require('axios');
+const fs = require('node:fs')
+const path = require('path')
 
 var router = express.Router();
 
+console.log(__dirname)
 const httpStatusCodes = [
-  "100", "101", "102", "103",
-  "200", "201", "202", "203", "204", "205", "206", "207", "208", "226",
-  "300", "301", "302", "303", "304", "305", "306", "307", "308",
-  "400", "401", "402", "403", "404", "405", "406", "407", "408", "409", "410",
-  "411", "412", "413", "414", "415", "416", "417", "418", "421", "422", "423", 
-  "424", "425", "426", "428", "429", "431", "451",
-  "500", "501", "502", "503", "504", "505", "506", "507", "508", "510", "511"
+  100,101,102,103,200,201,202,203,204,205,206,207,208,300,301,302,303,304,305,308,400,401,403,404,405,406,408,409,410,412,413,414,417,418,421,425,426,428,429,451,502,503,506,508,510,511
 ]
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  var item = httpStatusCodes[Math.floor(Math.random()*httpStatusCodes.length)];
-  const imageUrl = `https://http.cat/${item}.jpg`; // Hardcoded cat image URL
-  console.log("reading image")
-  axios
-    .get(imageUrl, {
-      responseType: 'arraybuffer'
-    })
-    .then((response) => {
-      // Set the content type to image/jpeg (or png, depending on the image type)
-      res.setHeader('Content-Type', 'image/jpeg');
-            
-      // Send the image data as the response
-      res.send(response.data);
-    })
+  try {
+    var item = httpStatusCodes[Math.floor(Math.random()*httpStatusCodes.length)];
+    console.log("reading image")
+  
+    const data = fs.readFileSync(path.resolve(__dirname, `../cat_images/${item}.jpg`), null);
+    
+    res.setHeader('Content-Type', 'image/jpeg');
+          
+    res.send(data);
+  } 
+  catch(e) {
+    console.log(e)
+  }
 });
 
 module.exports = router;
